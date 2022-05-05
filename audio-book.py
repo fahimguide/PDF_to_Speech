b6cc03d31c0070515dcde80d4d034af153bf6a06
fahimaziz <fahimazizguide@gmail.com>
2022-05-05 14:47:05 -0400

    First version

--- audio-book.py
+++ audio-book.py
@@ -1,6 +1,7 @@
 # importing needed libraries
 
-import pyttsx3, pdfplumber, PyPDF2
+import pyttsx3, pdfplumber, PyPDF2,os
+from gtts import gTTS
 
 # get the fle name
 file = r"C:\Users\Administrateur\PycharmProjects\pythonProject\Pandas 1.x Cookbook.pdf"
@@ -15,10 +16,14 @@ total_pages = file_reader.numPages
 # create pdf plumber and loop throw the number of pages
 
 with pdfplumber.open(file) as pdf:
+    text = ''
     for i in range(0,total_pages):
         page = pdf.pages[i]
-        text = page.extract_text()
-        print(text)
-        speaker = pyttsx3.init()
-        speaker.say(text)
-        speaker.runAndWait()
+        text += page.extract_text()
+final_output = gTTS(text=text, lang='en')
+print('generating speech')
+final_output.save('Pandas 1.x Cookbook.mp3')
+print('creating audio')
+os.system('Pandas 1.x Cookbook.mp3')
+
+
